948826549cb17d82377c071547396b192c6c27fb
LeeJaeHyuong <wozlgud@naver.com>
2023-02-07 16:57:29 +0900

    헬로월드 문구 추가

--- test.java
+++ test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
